760da7bc2f12012f8b003b987d9b179aa4f6087c
Suyan-Jian <2911719785@qq.com>
2025-03-03 20:21:12 +0800

    modified:   Script/bilibili_fake.js

--- Script/bilibili_fake.js
+++ Script/bilibili_fake.js
@@ -1,2 +1,7 @@
-console.log($request.url)
-console.log($argument.uid)
+let url = $request.url;
+let args = $argument;
+
+console.log("Request URL:", url);
+console.log("Arguments:", args);
+
+$done();
