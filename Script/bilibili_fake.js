b9c045053bdb2559ed96daaf50eb4c638c4901cb
nathalie <140290637+nathalie-Annis@users.noreply.github.com>
2025-03-04 17:52:21 +0800

    modified:   Script/bilibili_fake.js

--- Script/bilibili_fake.js
+++ Script/bilibili_fake.js
@@ -4,25 +4,30 @@ let id = idMatch ? idMatch[1] : null;
 console.log(id);
 let uids = $argument.uid ? $argument.uid.split(',') : [];
 console.log(uids);
-// 如果 id 与 uid 匹配，则继续执行脚本逻辑
+
+// 如果 id 与 uids 数组中任何一个uid匹配，则执行伪装逻辑
 if (id && uids.length > 0 && uids.includes(id)) {
     console.log("正在访问启用伪装的用户的个人空间");
-    // 伪装用户信息
     let obj = JSON.parse($response.body);
+
     // 伪装会员卡片标识
     if(obj.data.card.vip){
+        // 当前仅当vipType和vipStatus为以下字段时,才会显示会员标识
         obj.data.card.vip.vipType = 2;
         obj.data.card.vip.vipStatus = 1;
         if (obj.data.card.vip.label) {
             obj.data.card.vip.label.text = "百年大会员";
+            //以下显示图片可以任意替换
             obj.data.card.vip.label.image = "https://i0.hdslb.com/bfs/bangumi/kt/c43d9f30d0026fb3bba3d3823dd0f20c7ccc4f62.png";
             obj.data.card.vip.label.label_theme = "hundred_annual_vip";
         }
     }
+
     // 硬核会员出题字段修改
     if (obj.data.card.level_info.senior_inquiry){
         obj.data.card.level_info.senior_inquiry.inquiry_text = "硬核会员";
     }
+
     // 若用户顶部展示装扮收藏,伪装装扮收藏编号
     if (Array.isArray(obj?.data?.images?.collection_top_simple?.top?.result)) {
         obj.data.images.collection_top_simple.top.result.forEach(item => {
@@ -31,10 +36,36 @@ if (id && uids.length > 0 && uids.includes(id)) {
             }
         });
     }
-    // 伪装会员头像标识
+
+    if (obj.data.card) {
+        // 伪装粉丝数和点赞数
+        obj.data.card.fans = 5200000;
+        if(obj.data.card.likes){
+            obj.data.card.likes.like_num = 13140000;
+        }
+        // 伪装成就勋章粉丝勋章
+        obj.data.card.achieve={
+            "is_default": false,
+            "image": "https://i2.hdslb.com/bfs/face/27a952195555e64508310e366b3e38bd4cd143fc.png",
+            "achieve_url": `https://www.bilibili.com/h5/achieve?navhide=1&mid=${id}`
+        };
+        // console.log(`https://www.bilibili.com/h5/achieve?navhide=1&mid=${id}`);
+        obj.data.card.live_fans_wearing={
+            "medal_color_end": 15304379,
+            "medal_color_start": 7996451,
+            "medal_name": "彼岸花",
+            "level": 66,
+            "medal_jump_url": `https://live.bilibili.com/p/html/live-fansmedal-wall/index.html?is_live_webview=1&tId=${id}#/medal`,
+            "medal_color_border": 16771156,
+            "guard_icon": "https://i0.hdslb.com/bfs/activity-plat/static/ce06d65bc0a8d8aa2a463747ce2a4752/FqYoOmgssP.png"
+        };
+        // console.log(`https://live.bilibili.com/p/html/live-fansmedal-wall/index.html?is_live_webview=1&tId=${id}#/medal`);
+    }
+    
+    // 伪装会员头像标识(逻辑非常复杂,有bug/随着更新失效很正常,本人能力精力有限,请见谅)
+    // 头像框的存在会导致头像大小位置不同,需要根据是否存在头像框修改不同的样式
+    // 有头像框又分两种情况:静态头像框和动态头像框,两种头像框的特征为没有placeholder属性,src_type为1
     const layers = obj?.data?.card?.avatar?.fallback_layers?.layers || [];
-    // 头像框的存在会导致头像大小位置不同,所以需要判断是否存在头像框
-    // 两种情况:静态头像框和动态头像框,两种头像框的特征均为没有placeholder属性,src_type为1
     const exists = layers.some(item => 
         item?.resource?.res_type === 3 &&
         item?.resource?.res_image?.image_src && // 确保不是undefined
@@ -45,7 +76,10 @@ if (id && uids.length > 0 && uids.includes(id)) {
         item.resource.res_animation.webp_src.src_type === 1 &&
         !('placeholder' in item.resource.res_animation.webp_src)
     );
+
+    // 如果存在头像框
     if(exists){
+        // 首先统一不同用户的头像样式,使得没有数字头像的用户头像大小位置与有数字头像的用户一致
         obj.data.card.avatar.container_size={"width": 1.65,"height": 1.65};
         obj.data.card.avatar.fallback_layers.layers[0].general_spec={
         "pos_spec": {
@@ -103,9 +137,11 @@ if (id && uids.length > 0 && uids.includes(id)) {
             "opacity": 1
         }
         };
+        // 以上是所有用户都有的一些基本样式,再之后的样式会因为不同用户的会员/up主状态而有所不同,为了同一样式,需要将后面的样式全部删除，重新添加
         for (let i = 3; i < obj.data.card.avatar.fallback_layers.layers.length; i++) {
             delete obj.data.card.avatar.fallback_layers.layers[i];
         }
+        //  统一所有用户的图标为数字藏品图标+超大会员图标
         obj.data.card.avatar.fallback_layers.layers[3]={
         "resource": {
             "res_type": 5,
@@ -263,6 +299,8 @@ if (id && uids.length > 0 && uids.includes(id)) {
         "visible": true
         };
     }
+
+    // 如果不存在头像框,逻辑和上面的一样,只是样式不同
     else{
         obj.data.card.avatar.fallback_layers.layers[0].general_spec.size_spec={"width": 1.25,"height": 1.25};
         obj.data.card.avatar.fallback_layers.layers[1].general_spec.size_spec={"width": 1.2,"height": 1.2};
@@ -427,30 +465,7 @@ if (id && uids.length > 0 && uids.includes(id)) {
         "visible": true
         };
     }
-    if (obj.data.card) {
-        // 伪装粉丝数和点赞数
-        obj.data.card.fans = 5200000;
-        if(obj.data.card.likes){
-            obj.data.card.likes.like_num = 13140000;
-        }
-        // 伪装成就勋章粉丝勋章
-        obj.data.card.achieve={
-            "is_default": false,
-            "image": "https://i2.hdslb.com/bfs/face/27a952195555e64508310e366b3e38bd4cd143fc.png",
-            "achieve_url": `https://www.bilibili.com/h5/achieve?navhide=1&mid=${id}`
-        };
-        // console.log(`https://www.bilibili.com/h5/achieve?navhide=1&mid=${id}`);
-        obj.data.card.live_fans_wearing={
-            "medal_color_end": 15304379,
-            "medal_color_start": 7996451,
-            "medal_name": "彼岸花",
-            "level": 66,
-            "medal_jump_url": `https://live.bilibili.com/p/html/live-fansmedal-wall/index.html?is_live_webview=1&tId=${id}#/medal`,
-            "medal_color_border": 16771156,
-            "guard_icon": "https://i0.hdslb.com/bfs/activity-plat/static/ce06d65bc0a8d8aa2a463747ce2a4752/FqYoOmgssP.png"
-        };
-        // console.log(`https://live.bilibili.com/p/html/live-fansmedal-wall/index.html?is_live_webview=1&tId=${id}#/medal`);
-    }
+
     console.log("伪装完成");
     $done({ body: JSON.stringify(obj) });
 } else {
