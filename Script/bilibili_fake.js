63c4e729715776ebd0d30388aac9e28143c3f35d
nathalie <140290637+nathalie-Annis@users.noreply.github.com>
2025-03-04 12:39:42 +0800

    modified:   Script/bilibili_fake.js

--- Script/bilibili_fake.js
+++ Script/bilibili_fake.js
@@ -68,6 +68,49 @@ if (id && uids.length > 0 && uids.includes(id)) {
         },
         "visible": true
         };
+        obj.data.card.avatar.fallback_layers.layers[5]={
+        "resource": {
+            "res_type": 5,
+            "res_native_draw": {
+            "draw_src": {
+                "draw": {
+                "fill_mode": 1,
+                "draw_type": 1,
+                "color_config": {
+                    "day": {
+                    "argb": "#FFFFFFFF"
+                    },
+                    "night": {
+                    "argb": "#FF17181A"
+                    },
+                    "is_dark_mode_aware": true
+                }
+                },
+                "src_type": 3
+            }
+            }
+        },
+        "general_spec": {
+            "pos_spec": {
+            "axis_x": 0.845,
+            "axis_y": 1.1116666666666666,
+            "coordinate_pos": 1
+            },
+            "size_spec": {
+            "width": 0.275,
+            "height": 0.275
+            },
+            "render_spec": {
+            "opacity": 1
+            }
+        },
+        "layer_config": {
+            "tags": {
+            "ICON_LAYER": {}
+            }
+        },
+        "visible": true
+        };
         obj.data.card.avatar.fallback_layers.layers[6]={
         "resource": {
             "res_type": 4,
