2fbed2931064196ad9a93327fe31c85e741b080b
nathalie <140290637+nathalie-Annis@users.noreply.github.com>
2025-03-10 22:53:04 +0800

    modified:   Plugin/qimao.plugin modified:   Script/qimao_coin.js

--- Script/qimao_coin.js
+++ Script/qimao_coin.js
@@ -3,15 +3,8 @@ if (!$response.body) {
     $done({});
 }
 let json = JSON.parse($response.body);
-
 let hideCoin = $argument.hideCoin;
-if (typeof hideCoin === 'string') {
-    console.log('hideCoin 是字符串');
-} else if (typeof hideCoin === 'boolean') {
-    console.log('hideCoin 是布尔值');
-} else {
-    console.log('hideCoin 是其他类型');
-}
+let nickname = $argument.nickname;
 
 // 修改VIP信息
 if (json?.data?.user_area?.vip_info?.vip_open_info) {
@@ -26,7 +19,9 @@ if (json?.data?.user_area?.base_info) {
         json.data.user_area.base_info.user_other_data[0].num = "9999999";
     }
     json.data.user_area.base_info.level_text = "50";
-    json.data.user_area.base_info.nickname = "我是昵称";
+    if (nickname !== "") {
+        json.data.user_area.base_info.nickname = nickname;
+    }
     json.data.user_area.base_info.is_vip = "ODYxNjU2NDI5OTM5MzU1NkgqHtLCbFbu2ZpYZWZju3vXXGg6+PwVxDNDBV14nQDA";
     json.data.user_area.base_info.level_icon = "https://cdn.wtzw.com/bookimg/free/images/app/1_0_0/level/level_icon_50.png";
 }
@@ -38,11 +33,29 @@ if (json?.data?.user_area?.vip_info?.activity_info) {
 try {
     // 删除func_area中的特定元素
     if (json?.data?.func_area) {
-        json.data.func_area = json.data.func_area.filter(item => item?.type !== "banner" && item?.type !== "ads");
-    }
-    if (json?.data?.func_area?.length > 3 && json?.data.func_area[3]?.list) {
-        let list = json.data.func_area[3].list;
-        json.data.func_area[3].list = [list?.[0], list?.[3], list?.[5], list?.[6]].filter(Boolean);
+        // 使用 filter 方法过滤出需要保留的元素
+        json.data.func_area = json.data.func_area.filter(item => {
+            if (item?.type === "banner") {
+                return false;  // 删除类型为 "banner" 的元素
+            }
+            if (item?.type === "ads") {
+                if (hideCoin) {
+                    return false;  // 如果 hideCoin 为 true，删除类型为 "ads" 的元素
+                } else {
+                    // 修改 item.list，过滤出不包含 "防诈骗指南" 的元素
+                    item.list = item.list.filter(subItem => subItem?.discover_name !== "防诈骗指南");
+                    return true;  // 保留当前 "ads" 类型的元素
+                }
+            }
+            if (item?.type === "other") {
+                // 修改 item.list，保留特定 discover_name 的项
+                item.list = item.list.filter(subItem =>
+                    ["我的评论", "阅读喜好", "帮助与反馈", "设置"].includes(subItem?.discover_name)
+                );
+                return true;  // 保留当前 "other" 类型的元素
+            }
+            return true;  // 保留其他类型的元素
+        });
     }
 } catch (e) {
     console.log(e);
