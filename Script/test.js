79cda32ebdca657c1129fb19a27d714a67d43c89
Suyan-Jian <2911719785@qq.com>
2025-03-10 21:34:21 +0800

    modified:   Plugin/369cx.plugin modified:   Plugin/qimao.plugin new file:   Script/qimao_coin.js new file:   Script/qimao_interaction.js deleted:    Script/test.js

--- Script/test.js
+++
@@ -1,9 +0,0 @@
-let body = $response.body;
-if (body) {
-    let obj = JSON.parse(body);
-    if (obj?.result?.notice) {
-        delete obj.result.notice;
-    }
-    $done({ body: JSON.stringify(obj) });
-}
-$done({});
