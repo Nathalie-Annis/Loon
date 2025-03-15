5e14b6fc00bbf242d9e46e16875c9e29bb6096e6
nathalie <140290637+nathalie-Annis@users.noreply.github.com>
2025-03-15 13:57:31 +0800

    modified:   Script/qimao_search2.js

--- Script/qimao_search2.js
+++ Script/qimao_search2.js
@@ -3,17 +3,30 @@ if (!$response.body) {
     $done({});
 }
 
+let json = JSON.parse($response.body);
+if (json?.data?.search_hot_list) {
+    const order = ["热搜书籍", "热门话题", "有声热播"];
+    json.data.search_hot_list.sort((a, b) => order.indexOf(a.title) - order.indexOf(b.title));
+}
+
 let searchOption = $argument.searchOption;
 if (searchOption === "空白") {
-    let json = JSON.parse($response.body);
     if (json?.data?.search_dispose?.content) {
         json.data.search_dispose.content = " ";
     }
+    if (json?.data?.search_hot_tags) {
+        delete json.data.search_hot_tags;
+    }
     $done({ body: JSON.stringify(json) });
 } else if (searchOption === "默认") {
-    let json = JSON.parse($response.body);
     if (json?.data?.search_dispose?.content) {
         delete json.data.search_dispose;
+    }
+    if (json?.data?.search_hot_tags?.hot_words) {
+        json.data.search_hot_tags.title = "搜索发现";
+        json.data.search_hot_tags.hot_words = json.data.search_hot_tags.hot_words
+            .filter(item => item.title === "更多分类>")
+            .map(item => ({ ...item, title: "选择分类>" }));
     }
     $done({ body: JSON.stringify(json) });
 } else {
