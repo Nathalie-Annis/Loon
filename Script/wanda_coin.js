b54636d9b531ce6e85933ad11c031d554f669dcf
nathalie <140290637+nathalie-Annis@users.noreply.github.com>
2025-03-16 16:57:28 +0800

    modified:   Plugin/wandafilm.plugin modified:   Script/wanda_coin.js

--- Script/wanda_coin.js
+++ Script/wanda_coin.js
@@ -1,1 +1,10 @@
-$done({ body: JSON.stringify({}) });
+if (!$response.body) {
+    console.log("响应体为空");
+    $done({});
+}
+
+let json = JSON.parse($response.body);
+if (json?.data) {
+    json.data.expiredPointsRemindContent = "";
+}
+$done({ body: JSON.stringify(json) });
