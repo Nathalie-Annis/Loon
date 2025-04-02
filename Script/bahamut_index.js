17340dce18fafe39e320a993a2ac6485f73b18ad
Suyan-Jian <2911719785@qq.com>
2025-04-02 14:29:42 +0800

    modified:   Plugin/bahamut.plugin deleted:    Script/bahamut_announce.js new file:   Script/bahamut_index.js

---
+++ Script/bahamut_index.js
@@ -0,0 +1,17 @@
+let hideAnnounce = $argument.hideAnnounce;
+let hidePromotion = $argument.hidePromotion;
+if (!$response.body || !hideAnnounce && !hidePromotion) {
+    console.log('无需脚本处理');
+    $done({});
+}
+
+let json = JSON.parse($response.body);
+if (hideAnnounce && json?.data?.announce) {
+    console.log('隐藏公告:' + json.data.announce);
+    delete json.data.announce;
+}
+if (hidePromotion && json?.data?.promotion) {
+    console.log('隐藏新番导视入口');
+    delete json.data.promotion;
+}
+$done({ body: JSON.stringify(json) });
