34d482a4ab2e4bc082b3fc91e2947e5bad8f4712
nathalie <140290637+nathalie-Annis@users.noreply.github.com>
2025-04-02 14:29:42 +0800

    modified:   Plugin/bahamut.plugin deleted:    Script/bahamut_announce.js new file:   Script/bahamut_index.js

--- Script/bahamut_announce.js
+++
@@ -1,9 +0,0 @@
-let body = $response.body;
-if (body) {
-    let obj = JSON.parse(body);
-    if(obj.data && obj.data.announce){
-        delete obj.data.announce;
-    }
-    $done({body: JSON.stringify(obj)});
-}
-$done({});
