ab9d82bbb86280c989f48b5f7d326cc0da7e4bcd
Suyan-Jian <2911719785@qq.com>
2025-04-06 11:19:57 +0800

    modified:   Script/bahamut_sign_ad.js

--- Script/bahamut_sign_ad.js
+++ Script/bahamut_sign_ad.js
@@ -9,9 +9,7 @@ if (json?.data?.finished === 0) {
     $notification.post("巴哈姆特", "开始广告签到", "等待30秒后完成签到", "crazyanime://", 0);
 
     let url = $request.url.replace("start", "finished");
-    console.log(url);
     let headers = $request.headers;
-    console.log(headers);
     var params = {
         url: url,
         timeout: 5000,
@@ -21,17 +19,30 @@ if (json?.data?.finished === 0) {
 
     // 延迟30秒后发起请求，并在请求完成后调用 $done()
     setTimeout(() => {
-        $httpClient.get(params, function (error, response, data) {
+        $httpClient.get(params, function (error, _, data) {
             if (error) {
-                console.log('广告签到失败:' + error);
+                console.log('广告签到失败:\n' + error);
                 $notification.post("巴哈姆特", "广告签到失败", error, "crazyanime://", 0);
             } else {
-                console.log('广告签到成功:' + data);
-                $notification.post("巴哈姆特", "广告签到完成", "今日获得双倍巴币", "crazyanime://", 0);
+                json = JSON.parse(data);
+                if (json?.data?.finished === 1) {
+                    console.log('广告签到完成');
+                    $notification.post("巴哈姆特", "广告签到完成", "今日获得双倍巴币", "crazyanime://", 0);
+                }
+                else {
+                    console.log('广告签到失败:\n' + data);
+                    $notification.post("巴哈姆特", "广告签到失败", data, "crazyanime://", 0);
+                }
+                $done({ body: data }); // 延迟结束脚本
             }
-            $done({}); // 延迟结束脚本
         });
     }, 30000); // 等待30秒
 } else {
+    if (json?.data?.finished === 1) {
+        console.log('今日已领取过双倍签到奖励');
+    }
+    else {
+        console.log(json);
+    }
     $done({});
 }
