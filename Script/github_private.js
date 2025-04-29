ea524c2a93fae49b2533cf06e430bcbcf4073250
nathalie <140290637+nathalie-Annis@users.noreply.github.com>
2025-04-29 17:18:18 +0800

    modified:   Script/github_private.js

--- Script/github_private.js
+++ Script/github_private.js
@@ -1,7 +1,7 @@
 /**
 允许引用Github私有仓库/gist中的订阅分流，重写，脚本等配置文件。
 修改自https://raw.githubusercontent.com/Peng-YM/QuanX/master/Rewrites/GithubPrivate/github-private-repo.js
-1️⃣ 登陆Github > 点击头像下拉菜单 > 选择Settings > 左边菜单栏选择最后一个Developer settings > 选择Personal access tokens > Generate new token > Note里面填写token名字 > ☑️下面的勾选框选择第一项repo打钩（所有子项目自动勾选）> 点击Generate token按钮保存并返回。
+1️⃣ 登陆Github > 点击头像下拉菜单 > 选择Settings > 左边菜单栏选择最后一个Developer settings > 选择Personal access tokens > 选择Tokens(classic) > Generate new token > Generate new token(classic) > Note里面填写token名字,Expiration(token有效期)自行决定 > ☑️下面的勾选框选择第一项repo打钩（所有子项目自动勾选）> 点击Generate token按钮后GitHub会生成token,请妥善保存好,之后忘记了只能重新生成。
 2️⃣ 在Loon插件中里面填入用户名（打开Github，浏览器地址栏应该会显示https://github.com/这里是你的用户名/）和上面的token。
 */
 
